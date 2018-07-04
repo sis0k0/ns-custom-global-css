183c1f61ea535d355077e4799977b266bc6be871
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-04 17:09:07 +0300

    refactor: import the custom css loading script in main.aot.ts as well

--- app/main.aot.ts
+++ app/main.aot.ts
@@ -1,3 +1,5 @@
+import "./loadCss";
+
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
 import { platformNativeScript } from "nativescript-angular/platform-static";
 
