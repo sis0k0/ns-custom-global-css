9427e5b564b17b7abe6b9ff30fc874d1cc1b9758
sis0k0 <stanimira.vlaeva@gmail.com>
2018-07-04 17:08:16 +0300

    feat: add a custom script for loading the app1.css

---
+++ app/loadCss.ts
@@ -0,0 +1,7 @@
+const application = require("tns-core-modules/application");
+require("tns-core-modules/ui/styling/style-scope");
+
+global.registerModule("./app.css", () => require("~/app1.css"));
+
+application.loadAppCss();
+
--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,6 @@
 // this import should be first in order to load some required settings (like globals and reflect-metadata)
+import "./loadCss";
+
 import { platformNativeScriptDynamic } from "nativescript-angular/platform";
 
 import { AppModule } from "./app.module";
